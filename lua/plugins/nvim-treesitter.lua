ac26018ba1cca093b0e2e17db62de976b3d1196f
Mike Fischer <mikefischer99@gmail.com>
2023-05-08 15:13:14 -0400

    Stop the annoying plugin update notifications on every launch

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,3 +1,8 @@
+if true then
+  return {}
+end
+
+-- See: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/extras/lang/json.lua
 return {
   -- Custom list of treesitter parsers.
   -- This replaces any existing opts list.

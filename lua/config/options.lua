63fa728e0a8a45dc01dd4e455d1ea1413e8fec59
Mike Fischer <mikefischer99@gmail.com>
2023-08-23 15:13:47 -0400

    Minor tweaks

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,7 +8,8 @@ local opt = vim.opt
 vim.g.gh_color = "soft" -- slightly less dark background for vim-github-dark theme
 vim.o.swapfile = false -- don't save autorecovery files
 
-opt.guifont = { "Rec Mono Casual", ":h14" } -- for Neovide or other gui editors
+-- opt.guifont = { "Rec Mono Casual:h14" } -- for Neovide or other gui editors
+opt.guifont = { "FiraCode Nerd Font Mono:h14" } -- for Neovide or other gui editors
 opt.undofile = false -- Do not persist undo after quit
 
 opt.clipboard = "" -- Don't use system clipboard... it just clutters it up
--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -3,6 +3,7 @@ return {
   {
     "max397574/better-escape.nvim",
     event = "InsertCharPre",
+    enabled = true,
     config = function()
       require("better_escape").setup()
     end,
